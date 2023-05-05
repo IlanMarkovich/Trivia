2a95989896cb4fbc98a9eab25674f87fb6e57263
IlanMark <ilanm0309@gmail.com>
2023-05-05 20:00:52 +0300

    Create `Server` class and implement the `main` function of the project

---
+++ TriviaServer/Server.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Server
+{
+public:
+	/// <summary>
+	/// The main method of the server
+	/// which activates the communicator between the server and the clients
+	/// </summary>
+	void run();
+};
