0b6c00037838b06ab9dd2e7744a52ab6d17461be
IlanMark <ilanm0309@gmail.com>
2023-05-09 10:02:44 +0300

    Add method signatures and structs in `IRequestHandler` class.

--- TriviaServer/IRequestHandler.h
+++ TriviaServer/IRequestHandler.h
@@ -1,5 +1,26 @@
 #pragma once
 
+#include <ctime>
+#include <vector>
+
+using std::vector;
+
+struct RequestInfo
+{
+	unsigned int id;
+	time_t receivalTime;
+	vector<unsigned char> buffer;
+};
+
+struct RequestResult
+{
+	vector<unsigned char> response;
+	IRequestHandler* newHandler;
+};
+
 class IRequestHandler
 {
+public:
+	virtual bool isRequestRelevant(RequestInfo info) = 0;
+	virtual RequestResult handleRequest(RequestInfo info) = 0;
 };
