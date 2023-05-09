25f738c2b6e290c3c9ebfd3d4f55ac385b3a31c4
IlanMark <ilanm0309@gmail.com>
2023-05-09 11:29:38 +0300

    Fix the `createBuffer` method in the serializer class.

--- TriviaServer/JsonResponsePacketSerializer.cpp
+++ TriviaServer/JsonResponsePacketSerializer.cpp
@@ -4,7 +4,27 @@
 
 vector<unsigned char> JsonResponsePacketSerializer::createBuffer(std::function<json()> serRes, unsigned char code)
 {
-    return vector<unsigned char>();
+    vector<unsigned char> buffer = { code };
+
+    string data = serRes().dump();
+    int len = data.size();
+
+    while (len != 0)
+    {
+        // Enter the byte to the buffer
+        buffer.push_back(len & 0xFF);
+
+        // Shift the variable 8 bits (1 byte)
+        len >>= 8;
+    }
+
+    // Reverse the length (so it will be the original value)
+    std::reverse(buffer.begin() + 1, buffer.end());
+    
+    // Insert the data of the response
+    buffer.insert(buffer.end(), data.begin(), data.end());
+
+    return buffer;
 }
 
 // PUBLIC METHODS
