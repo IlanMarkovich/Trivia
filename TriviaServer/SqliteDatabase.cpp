ffe2e7f66ad1bfab1ddd5ae20d7f2c911978dc37
IlanMark <ilanm0309@gmail.com>
2023-05-11 18:07:57 +0300

    Implement the open() and close() methods in SqliteDatabase class.

--- TriviaServer/SqliteDatabase.cpp
+++ TriviaServer/SqliteDatabase.cpp
@@ -1,1 +1,41 @@
 #include "SqliteDatabase.h"
+
+#include <iostream>
+#include <io.h>
+
+bool SqliteDatabase::open()
+{
+    // Check if database file already exists
+    int file_exists = _access(DATABASE_NAME, 0);
+    int result = sqlite3_open(DATABASE_NAME, &_db);
+
+    if (result != SQLITE_OK)
+    {
+        std::cerr << "Failed to open database!" << std::endl;
+        return false;
+    }
+
+    // There is no existing database, so we need to create a new one
+    if (file_exists != 0)
+    {
+        string tableQuery = "create table users(name text primary key not null, password text not null, email text not null);";
+        char* errMsg = nullptr;
+        int queryResult = sqlite3_exec(_db, tableQuery.c_str(), nullptr, nullptr, &errMsg);
+
+        if (queryResult != SQLITE_OK)
+        {
+            std::cerr << "Failed to create new table! more info: " << errMsg << std::endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool SqliteDatabase::close()
+{
+    int result = sqlite3_close(_db);
+    _db = nullptr;
+
+    return result == SQLITE_OK;
+}
--- TriviaServer/SqliteDatabase.h
+++ TriviaServer/SqliteDatabase.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "IDatabase.h"
+#include "sqlite3.h"
+
+#define DATABASE_NAME "triviaDB"
 
 class SqliteDatabase : public IDatabase
 {
@@ -10,4 +13,7 @@ public:
 	virtual int doesUserExist(string username) override;
 	virtual int doesPasswordMatch(string username, string password) override;
 	virtual int addNewUser(string username, string password, string email) override;
+
+private:
+	sqlite3* _db;
 };
