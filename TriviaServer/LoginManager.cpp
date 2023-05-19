aed5facd2cb5be1796d9514aab940021c971bd23
IlanMark <ilanm0309@gmail.com>
2023-05-19 19:55:09 +0300

    Add check when logging in (check that the user isn't already logged in)

--- TriviaServer/LoginManager.cpp
+++ TriviaServer/LoginManager.cpp
@@ -20,8 +20,12 @@ bool LoginManager::signup(string username, string password, string email) const
 
 bool LoginManager::login(string username, string password)
 {
-	// If the user exists and the password is correct, add this user to the logged users
-	if (_database->doesUserExist(username) && _database->doesPasswordMatch(username, password))
+	bool isLoggedIn = std::count_if(_loggedUsers.begin(), _loggedUsers.end(), [username](const LoggedUser& user) {
+		return user.getUsername() == username;
+		});
+
+	// If the user exists, the password is correct and the user isn't already logged in, add this user to the logged users
+	if (_database->doesUserExist(username) && _database->doesPasswordMatch(username, password) && !isLoggedIn)
 	{
 		_loggedUsers.push_back(LoggedUser(username));
 		return true;
@@ -32,8 +36,8 @@ bool LoginManager::login(string username, string password)
 
 void LoginManager::logout(string username)
 {
-	// This removes the user from the logged user list, by username.
-	_loggedUsers.erase(std::remove_if(_loggedUsers.begin(), _loggedUsers.end(), [username](const LoggedUser& user) {
+	// This removes the user from the logged user list, by username
+	_loggedUsers.erase(std::find_if(_loggedUsers.begin(), _loggedUsers.end(), [username](const LoggedUser& user) {
 		return user.getUsername() == username;
-		}), _loggedUsers.end());
+		}));
 }
