2d3091879317260a0a505c22899a1a201f2d27a3
IlanMark <ilanm0309@gmail.com>
2023-06-08 23:46:45 +0300

    Implement public methods in RoomAdminRequestHandler class

---
+++ TriviaServer/RoomAdminRequestHandler.cpp
@@ -0,0 +1,37 @@
+#include "RoomAdminRequestHandler.h"
+
+// C'tor
+
+RoomAdminRequestHandler::RoomAdminRequestHandler(RequestHandlerFactory& handlerFactory, const LoggedUser& user, const Room& room)
+	: RoomMemberRequestHandler(handlerFactory, user, room)
+{
+}
+
+// PUBLIC METHODS
+
+bool RoomAdminRequestHandler::isRequestRelevant(RequestInfo info)
+{
+	return info.id >= CLOSE_ROOM && info.id <= GET_ROOM_STATE;
+}
+
+RequestResult RoomAdminRequestHandler::handleRequest(RequestInfo info)
+{
+	RequestResult result;
+
+	switch (info.id)
+	{
+		case CLOSE_ROOM:
+			result = closeRoom(info);
+			break;
+
+		case START_GAME:
+			result = closeRoom(info);
+			break;
+
+		case GET_ROOM_STATE:
+			result = getRoomState(info);
+			break;
+	}
+
+	return result;
+}
