cf0a4895374223e0aa6249cddc10c59128555911
IlanMark <ilanm0309@gmail.com>
2023-06-18 11:46:49 +0300

    Write header file for the GameRequestHandler class

---
+++ TriviaServer/GameRequestHandler.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "IRequestHandler.h"
+#include "RequestHandlerFactory.h"
+#include "GameManager.h"
+
+class GameRequestHandler : public IRequestHandler
+{
+private:
+	// FIELDS
+	Game& _game;
+	LoggedUser _user;
+	RequestHandlerFactory& _handlerFactory;
+
+	// METHODS
+	RequestResult getQuestion(RequestInfo info);
+	RequestResult submitAnswer(RequestInfo info);
+	RequestResult getGameResults(RequestInfo info);
+	RequestResult leaveGame(RequestInfo info);
+public:
+	// C'tor
+	GameRequestHandler(Game& game, const LoggedUser& user, RequestHandlerFactory& handlerFactory);
+
+	// METHODS
+	virtual bool isRequestRelevant(RequestInfo info) override;
+	virtual RequestResult handleRequest(RequestInfo info) override;
+};
